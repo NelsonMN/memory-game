a1759144cb86dcd8f1c7804f41bb897c43fe4a02
Nelson <nelson.mieszkalski@gmail.com>
2022-06-10 15:38:34 -0400

    Added state display in header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './styles/App.css';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   return (
     <div>
+      score: {score}, best score: {bestScore}
     </div>
-  )
-}
+  );
+};
 
 export default App;
